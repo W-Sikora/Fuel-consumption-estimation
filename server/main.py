40c4c20827e0bff8b98e8306285b011fb3cdc4d0
Qlas <39061220+Qlas@users.noreply.github.com>
2020-06-23 14:12:14 +0200

    final

--- server/main.py
+++ server/main.py
@@ -3,7 +3,6 @@ from tensorflow.keras.models import load_model
 import numpy as np
 
 app = Flask(__name__)
-data = 0
 model = load_model('network.h5')
 
 
@@ -13,20 +12,14 @@ def transform(array):
 
 @app.route('/foo', methods=['POST', 'GET'])
 def foo():
-    global data
-    data2 = request.json
-    data2 = data2['speed']
-    data2 = [float(i) for i in data2]
-    data2 = np.array(data2)
-    data2 = transform(data2)
-    data = round(float(model.predict(data2)[-1, 0]), 2)
-    return jsonify(data)
-
-
-@app.route('/api', methods=['POST', 'GET'])
-def api():
+    data = request.json
+    data = data['speed']
+    data = [float(i) for i in data]
+    data = np.array(data)
+    data = transform(data)
+    data = round(float(model.predict(data)[-1, 0]), 2)
     return jsonify(data)
 
 
 if __name__ == "__main__":
-    app.run(host='192.168.43.200')
+    app.run(host='192.168.1.17')
